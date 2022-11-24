bf0b84bae926fe25e0402946a7be74fe80c5ddda
Amy <Amy.L.Wilson@ed.ac.uk>
2022-11-24 22:27:20 +0000

    add lec 20 recording

--- static/js/links.js
+++ static/js/links.js
@@ -760,7 +760,7 @@ try {DateRestrictLink(document.getElementById("MHL19"), "21 November 2022", "htt
 
 try {DateRestrictLink(document.getElementById("lecture20"), "21 November 2022", "https://uoeids.github.io/slides/w10-L20/w10-L20.html");} catch { };
 try {DateRestrictLink(document.getElementById("GHL20"), "21 November 2022", "https://github.com/uoeIDS/uoeIDS.github.io/tree/main/slides/w10-L20");} catch { };
-try {DateRestrictLink(document.getElementById("MHL20"), "21 November 2022", "/error");} catch { };
+try {DateRestrictLink(document.getElementById("MHL20"), "21 November 2022", "https://echo360.org.uk/lesson/G_efe2d84a-4230-499d-9c17-c921a3e3494b_72dbeb50-863a-4fc5-8c6b-8f0be8a5ce6e_2022-11-24T10:00:00.000_2022-11-24T10:55:00.000/classroom#sortDirection=desc");} catch { };
 
 // week 11 lecture 20/21
 try {DateRestrictLink(document.getElementById("lecture21"), "28 November 2022", "https://uoeids.github.io/slides/w11-L21/w11-L21.html");} catch { };
